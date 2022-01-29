9c6ad12ef1b2564e39e13e77abe2a369262e4b7d
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-01-29 20:02:50 +0100

    Clear up description

--- src/ModelFactory.php
+++ src/ModelFactory.php
@@ -14,7 +14,7 @@ class ModelFactory
      */
     public static function fromArray(string|Model $model, array $data): Model
     {
-        /** @phpstan-ignore-next-line As PHPStan thinks the class-string is always a valid Model class, but that is not enforced yet */
+        /** @phpstan-ignore-next-line As PHPStan thinks the class-string is always a valid Model class, but that is not enforced by phpdoc annotations */
         if (is_subclass_of($model, Model::class, true) === false) {
             throw new InvalidArgumentException('Only Models extending "' . Model::class . '" can be constructed using this factory');
         }
